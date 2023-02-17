252d6e4caca425c7c5884dcf7ee20bd40eca41e1
GuruMahan <iosdeve2022@gmail.com>
2023-02-17 11:08:50 +0530

    Initial Commit

---
+++ ScaleMetricsPropertyWrapperPOC/ScaleMetricsPropertyWrapperPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScaleMetricsPropertyWrapperPOCApp.swift
+//  ScaleMetricsPropertyWrapperPOC
+//
+//  Created by Guru Mahan on 17/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ScaleMetricsPropertyWrapperPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
